13b9f3fe2d46c88dbde24d239e256570d6e104c3
Ilya Kurdyukov <59548320+ilyakurdyukov@users.noreply.github.com>
2024-07-11 18:36:58 +0700

    lzma decoder for fphelper

---
+++ fphelper/lzma/LzmaTypes.h
@@ -0,0 +1,12 @@
+#ifndef __LZMATYPES_H
+#define __LZMATYPES_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef uint8_t Byte;
+typedef uint16_t UInt16;
+typedef uint32_t UInt32;
+typedef size_t SizeT;
+
+#endif
